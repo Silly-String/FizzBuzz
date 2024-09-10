ef3d889d5f2c048623ace33cd03a48ba6eda6712
Sill <158129203+Silly-String@users.noreply.github.com>
2024-09-10 15:21:45 -0400

    Completed task 4 in 'read me'

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int counter = 0;
+
+        while (i < 1000) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
+
+                counter++;
+            }
+
+            i++;
+        }
+
+        System.out.println(counter);
+    }
 }
